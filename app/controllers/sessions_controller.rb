f682eece509ec545053d701969a48f3f96721c1c
bcaneba <bcaneba@gmail.com>
2017-04-23 17:48:54 -0500

    added foundation to the project for the front end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
+	def new
+	end
+	
+	def create
+		@user = User.find_by(:username => session_params[:username]).try(:authenticate, session_params[:password])
+		if @user
+			session[:user_id] = @user.id
+			redirect_to root_path
+		else
+			flash[:notice] = "Please enter correct credentials"
+			render 'sessions/new'
+		end
+	end
+	def destroy
+		session.clear
+		flash[:notice] = "Log out successful"
 
+		redirect_to root_path
+	end
+
+	private
+	def session_params
+		params.require(:session).permit(:username,:password)
+	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
 
+	
+
+
+
+
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :password, :first_name, :last_name, :email, :phone)
+	end
 
 end
