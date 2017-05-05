ddb9517a4b77e885f0c98ee2a7e737a161bb0c74
bcaneba <bcaneba@gmail.com>
2017-05-05 01:13:42 -0500

    added post controller and post controller spec test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
 
+	before_action :set_post, only: [:new,:create]
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+      flash[:notice] = 'Post has successfully been created.'
+			redirect_to school_course_path(@school,@course,@post,current_user)
+		else
+      flash[:notice] = 'Please enter all fields correctly.'
+			render 'new'
+		end
+	end
+
+	private
+
+	def set_post
+		@course = Course.find(params[:school_id])
+		@school = School.find(params[:course_id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :description, :user_id, :course_id, :school_id)
+	end
 end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe PostsController, :type => :controller do
+	render_views
+	
+	before(:each) do |example|
+		unless example.metadata[:skip_post_create]
+			@user = FactoryGirl.create(:user)
+			@course = FactoryGirl.create(:course)
+	  	@school = FactoryGirl.create(:school)
+	  	@post = FactoryGirl.create(:post)
+	  end
+	end
+
+	describe "GET new" do
+		it "renders the new form for a school" do
+			get :new, params: {user_id: @user.id, course_id: @course.id, school_id: @school.id}
+      expect(response).to render_template("new")
+		end
+	end
+
+	describe "POST create" do
+		it "render to the root if a school is successfully created" do
+			post :create, params: { post: FactoryGirl.attributes_for(:post), user_id: @user.id, course_id: @course.id, school_id: @school.id}
+      # expect(response).to be_success
+		end
+	end
+
+end
